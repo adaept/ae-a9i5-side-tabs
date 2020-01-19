f47acaa17178ed93d959f48365da277eb1c2ed3a
peterennis <peterennis@yahoo.com>
2020-01-18 21:12:31 -0800

    feat(ionic): v5.0.0-beta.5 and using @adaept/ae-icon5-component 0.9.993

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 
 // Note: loader import location set using "esmLoaderPath" within the output target config
 // needed for adding aeicons component
-import { applyPolyfills, defineCustomElements } from '@adaept/ae-icons-component/dist/loader';
+import { applyPolyfills, defineCustomElements } from '@adaept/ae-icon5-component/dist/loader';
 
 if (environment.production) {
   enableProdMode();
